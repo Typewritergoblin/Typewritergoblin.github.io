200d3593c714f423d89f6bf40048cc336d2b2c8b
Typewritergoblin <56444928+Typewritergoblin@users.noreply.github.com>
2021-02-11 20:41:00 -0700

    Update data_reaction.js

--- js/data_reaction.js
+++ js/data_reaction.js
@@ -42,7 +42,8 @@ data_reaction = [
         bullets: [
             "Trigger: enemy creature you can see attacks you.",
             "Roll an <strong>Athletics</strong> check with DC 15.",
-            "On a success, reduce the damage you take by your <strong>Guard</strong> value."
+            "On a success, reduce the damage you take by your <strong>Guard</strong> value.",
+            "Damage reduction takes place before any resistances are applied."
         ]
     },
     {
@@ -57,6 +58,37 @@ data_reaction = [
             "If you match or exceed the enemy attack roll, you take half damage from the attack.",
             "Critical Success: If you roll a critical success and the enemy does not, you automatically succeed.",
             "If you roll a critical success and match or exceed the enemy attack roll, you take no damage from the attack."
+        ]
+    },
+    {
+        title: "Spellshield",
+        icon: "divert",
+        subtitle: "Enemy casts a spell",
+        description: "Attempt to use magic to protect the target",
+        reference: "Icara Homebrew",
+        bullets: [
+            "Trigger: enemy creature you can see casts a spell.",
+            "Roll an <strong>Arcana(Trained)</strong> or <b>Channeling(Trained)</b> check against DC 15.",
+            "On a success, 1 target of the spell within 30' of you takes reduced damage from it",
+            "The damage is reduced by an amount equal to the spellcasting modifier of the skill check used.",
+            "If you have spell slots available, you can further reduce the damage by 1d6 per spell slot spent.",
+            "Damage reduction takes place before any resistances are applied."
+        ]
+    },
+    {
+        title: "Spellbreak",
+        icon: "magic-swirl",
+        subtitle: "Enemy casts a spell",
+        description: "Attempt to disrupt the enemy spell using your own magical energy",
+        reference: "Icara Homebrew",
+        bullets: [
+            "Trigger: A creature you can see within 60' casts a spell.",
+            "Roll an <strong>Arcana(Trained)</strong> or <b>Channeling(Trained)</b> check.",
+            "The DC equals 10 + (2 per lvl of the enemy spell), results vary by check",
+            "If you fail by more than 5, you lose available spell slots equal to the level of the enemy spell.",
+            "If you fail by 5 or less, you lose no spell slots and do not effect the enemy spell.",
+            "If you succeed, you may spend spell slots equal to enemy spell level to either halve its damage, grant all targets advantage on saves, or reduce its duration by half.",
+            "If you succeed by 5 or more, you may spend spell slots equal to the enemy spell level to cancel it altogether. The enemy loses their spell."
         ]
     },
 ]
