1ffd9cb9d9545f4b4140118c2c738f42e4e2c7ab
Typewritergoblin <56444928+Typewritergoblin@users.noreply.github.com>
2024-07-20 10:59:19 -0600

    Updated Actions
    Added some universal bonus actions
    Added non-specific restrictions to non-bonus actions

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -1,76 +1,97 @@
 data_bonusaction = [
-    {
-        title: "Offhand Attack",
-        icon: "crossed-swords",
-        subtitle: "Use with the Attack action",
-        description: "Attack with your off hand",
-        reference: "PHB, pgs. 192,194-195.",
-        bullets: [
-            "Only usable if you take the <b>Attack</b> action and attack with a light melee weapon that you're holding in one hand.",
-            "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
-            "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
-            "If either weapon has the thrown property, you can throw the weapon, instead of making a melee attack with it."
-        ]
-    },
-    {
-        title: "Guard",
-        icon: "shield-reflect",
-        subtitle: "You prepare to defend from an attack",
-        description: "Reduce the damage you take by steeling yourself for impact",
-        reference: "Icara Homebrew",
-        bullets: [
-            "You guard yourself from damage, based on your armor value.",
-            "The first physical damage attack that damages you before the beginning of your next turn is reduced by your <strong>Guard</strong> value.",
-            "Damage reduction takes place before any resistances are applied."
-        ]
-    },
-    {
-        title: "Ward",
-        icon: "divert",
-        subtitle: "You prepare to defend from a spell",
-        description: "Attempt to use magic to protect the target",
-        reference: "Icara Homebrew",
-        bullets: [
-            "You prepare to protect a target from malicious magic.",
-            "Roll an <strong>Arcana(Trained)</strong> or <b>Channeling(Trained)</b> check against DC 10.",
-            "On a success, 1 target within 30' of you takes reduced damage from the first spell that damages them before the start of your next turn.",
-            "The damage is reduced by an amount equal to the spellcasting modifier of the skill check used.",
-            "If you have spell slots available, you can bolster the damage reduction by 1d6 per spell slot spent.",
-            "Damage reduction takes place before any resistances are applied.",
-            "You must be trained in the skill you use to create the Ward."
-        ]
-    },
-    {
-        title: "Use Prepared Item",
-        icon: "snatch",
-        subtitle: "Use an available item",
-        description: "Use an item designed to be used quickly",
-        reference: "Icara Homebrew",
-        bullets: [
-            "You can interact with one object for free during your turn (such as drawing a weapon or opening a door). If using an item that is quickly available, use this item.",
-            "Items that can be grabbed and used with a single motion (or reasonably close to a single motion) can be used as prepared items.",
-            "Common prepared items are potions on the belt or in hand, poisons administered quickly on the blade, or throwing a handful of objects drawn from a belt or accessible hidden pouch."
-        ]
-    },
-    {
-        title: "Cast a spell",
-        icon: "magic-swirl",
-        subtitle: "Cast time of 1 bonus action",
-        description: "Cast a spell with a casting time of 1 bonus action",
-        reference: "PHB, pg. 192.",
-        bullets: [
-            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
-            "For further details, see the <b>Cast a spell</b> action."
-        ]
-    },
-    {
-        title: "Use class feature",
-        icon: "embrassed-energy",
-        subtitle: "Some features use bonus actions",
-        description: "Use a racial or class feature that uses a bonus action",
-        reference: "See class page for more information.",
-        bullets: [
-
-        ]
-    }
-]
+  {
+    title: "Offhand Attack",
+    icon: "crossed-swords",
+    subtitle: "Use with the Attack action",
+    description: "Attack with your off hand",
+    reference: "PHB, pgs. 192,194-195.",
+    bullets: [
+      "Only usable if you take the <b>Attack</b> action and attack with a light melee weapon that you're holding in one hand.",
+      "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
+      "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
+      "If either weapon has the thrown property, you can throw the weapon, instead of making a melee attack with it.",
+    ],
+  },
+  {
+    title: "Rushed Attack",
+    icon: "crossed-swords",
+    subtitle: "Melee attack",
+    description: "Perform an off-balance melee attack with a drawn weapon",
+    reference: "Icara Homebrew",
+    bullets: [
+      "Perform a single attack with a drawn weapon",
+      "This attack must always be made with disadvantage.",
+      "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
+    ],
+  },
+  {
+    title: "Dart",
+    icon: "sprint",
+    subtitle: "Move a small amount",
+    description: "Make a small quick action",
+    reference: "Icara Homebrew",
+    bullets: [
+      "Move 10' as a bonus action, after applying any condition or environment modifiers.",
+    ],
+  },
+  {
+    title: "Guard",
+    icon: "shield-reflect",
+    subtitle: "You prepare to defend from an attack",
+    description: "Reduce the damage you take by steeling yourself for impact",
+    reference: "Icara Homebrew",
+    bullets: [
+      "You guard yourself from damage, based on your armor value.",
+      "The first physical damage attack that damages you before the beginning of your next turn is reduced by your <strong>Guard</strong> value.",
+      "The default <strong>Guard</strong> is equal to 1 for light armor, 2 for medium armor, and 4 for heavy armor. If a shield is in your off-hand, increase the reduction by 1.",
+      "Damage reduction takes place before any resistances are applied.",
+    ],
+  },
+  {
+    title: "Ward",
+    icon: "divert",
+    subtitle: "You prepare to defend from a spell",
+    description: "Attempt to use magic to protect the target",
+    reference: "Icara Homebrew",
+    bullets: [
+      "You prepare to protect a target from malicious magic.",
+      "Special: You must have the <strong>Spell Casting</strong> feature and be trained in the skill you use to create the ward.",
+      "Roll an <strong>Arcana(Trained)</strong> or <b>Channeling(Trained)</b> check against DC 10.",
+      "On a success, 1 target within 30' of you takes reduced damage from the first spell that damages them before the start of your next turn.",
+      "The damage is reduced by an amount equal to the spellcasting modifier of the skill check used.",
+      "If you have spell slots available, you can bolster the damage reduction by 1d6 per spell slot spent.",
+      "Damage reduction takes place before any resistances are applied.",
+    ],
+  },
+  {
+    title: "Use Prepared Item",
+    icon: "snatch",
+    subtitle: "Use an available item",
+    description: "Use an item designed to be used quickly",
+    reference: "Icara Homebrew",
+    bullets: [
+      "You can interact with one object for free during your turn (such as drawing a weapon or opening a door). If using an item that is quickly available, use this item.",
+      "Items that can be grabbed and used with a single motion (or reasonably close to a single motion) can be used as prepared items.",
+      "Common prepared items are potions on the belt or in hand, poisons administered quickly on the blade, or throwing a handful of objects drawn from a belt or accessible hidden pouch.",
+    ],
+  },
+  {
+    title: "Cast a spell",
+    icon: "magic-swirl",
+    subtitle: "Cast time of 1 bonus action",
+    description: "Cast a spell with a casting time of 1 bonus action",
+    reference: "PHB, pg. 192.",
+    bullets: [
+      "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
+      "For further details, see the <b>Cast a spell</b> action.",
+    ],
+  },
+  {
+    title: "Use class feature",
+    icon: "embrassed-energy",
+    subtitle: "Some features use bonus actions",
+    description: "Use a racial or class feature that uses a bonus action",
+    reference: "See class page for more information.",
+    bullets: [],
+  },
+];
--- js/data_reaction.js
+++ js/data_reaction.js
@@ -1,66 +1,67 @@
 data_reaction = [
-    {
-        title: "Opportunity attack",
-        icon: "crossed-swords",
-        subtitle: "Enemy leaves your reach",
-        description: "You can rarely move heedlessly past your foes without putting yourself in danger",
-        reference: "PHB, pg. 195.",
-        bullets: [
-            "Trigger: enemy creature you can see leaves your reach.",
-            "Make one melee attack against the provoking creature.",
-            "The attack interrupts the provoking creature's movement, occurring right before the creature leaves your reach.",
-            "Creatures don't provoke an opportunity attack when they teleport or when someone or something moves them without using their movement, action, or reaction."
-        ]
-    },
-    {
-        title: "Readied action",
-        icon: "stopwatch",
-        subtitle: "Part of your Ready action",
-        description: "Execute the reaction specified by your Ready action",
-        reference: "PHB, pg. 193.",
-        bullets: [
-            "Trigger: specified by your <strong>Ready</strong> action."
-        ]
-    },
-    {
-        title: "Cast a spell",
-        icon: "magic-swirl",
-        subtitle: "Cast time of 1 reaction",
-        description: "Cast a spell with a casting time of 1 reaction",
-        reference: "PHB, pg. 192.",
-        bullets: [
-            "Trigger: specified by the spell.",
-            "For further details, see the <strong>Cast a spell</strong> action."
-        ]
-    },
-    {
-        title: "Parry",
-        icon: "sword-break",
-        subtitle: "Enemy attacks you",
-        description: "Attempt to deflect the enemy blow",
-        reference: "Icara Homebrew",
-        bullets: [
-            "Trigger: enemy creature you can attacks you.",
-            "Make one melee attack - compare the number to the enemy attack roll.",
-            "If you match or exceed the enemy attack roll, you take half damage from the attack.",
-            "Critical Success: If you roll a critical success and the enemy does not, you automatically succeed.",
-            "If you roll a critical success and match or exceed the enemy attack roll, you take no damage from the attack."
-        ]
-    },
-    {
-        title: "Spellbreak",
-        icon: "magic-swirl",
-        subtitle: "Enemy casts a spell",
-        description: "Attempt to disrupt the enemy spell using your own magical energy",
-        reference: "Icara Homebrew",
-        bullets: [
-            "Trigger: A creature you can see within 60' casts a spell.",
-            "Roll an <strong>Arcana(Trained)</strong> or <b>Channeling(Trained)</b> check.",
-            "The DC equals 10 + (2 per lvl of the enemy spell), results vary by check",
-            "If you fail by more than 5, you lose available spell slots equal to the level of the enemy spell.",
-            "If you fail by 5 or less, you lose no spell slots and do not effect the enemy spell.",
-            "If you succeed, you may spend spell slots equal to enemy spell level to either halve its damage, grant all targets advantage on saves, or reduce its duration by half.",
-            "If you succeed by 5 or more, you may spend spell slots equal to the enemy spell level to cancel it altogether. The enemy loses their spell."
-        ]
-    },
-]
+  {
+    title: "Opportunity attack",
+    icon: "crossed-swords",
+    subtitle: "Enemy leaves your reach",
+    description:
+      "You can rarely move heedlessly past your foes without putting yourself in danger",
+    reference: "PHB, pg. 195.",
+    bullets: [
+      "Trigger: enemy creature you can see leaves your reach.",
+      "Make one melee attack against the provoking creature.",
+      "The attack interrupts the provoking creature's movement, occurring right before the creature leaves your reach.",
+      "Creatures don't provoke an opportunity attack when they teleport or when someone or something moves them without using their movement, action, or reaction.",
+    ],
+  },
+  {
+    title: "Readied action",
+    icon: "stopwatch",
+    subtitle: "Part of your Ready action",
+    description: "Execute the reaction specified by your Ready action",
+    reference: "PHB, pg. 193.",
+    bullets: ["Trigger: specified by your <strong>Ready</strong> action."],
+  },
+  {
+    title: "Cast a spell",
+    icon: "magic-swirl",
+    subtitle: "Cast time of 1 reaction",
+    description: "Cast a spell with a casting time of 1 reaction",
+    reference: "PHB, pg. 192.",
+    bullets: [
+      "Trigger: specified by the spell.",
+      "For further details, see the <strong>Cast a spell</strong> action.",
+    ],
+  },
+  {
+    title: "Parry",
+    icon: "sword-break",
+    subtitle: "Enemy attacks you",
+    description: "Attempt to deflect the enemy blow",
+    reference: "Icara Homebrew",
+    bullets: [
+      "Trigger: enemy creature you can attacks you.",
+      "Special: Only available as part of a Readied Action.",
+      "Make one melee attack - compare the number to the enemy attack roll.",
+      "If you match or exceed the enemy attack roll, you take half damage from the attack.",
+      "If you succeed by 5 or more, you take no damage from the attack.",
+    ],
+  },
+  {
+    title: "Spellbreak",
+    icon: "magic-swirl",
+    subtitle: "Enemy casts a spell",
+    description:
+      "Attempt to disrupt the enemy spell using your own magical energy",
+    reference: "Icara Homebrew",
+    bullets: [
+      "Trigger: A creature you can see within 60' casts a spell.",
+      "Special: Only available as part of a Readied Action.",
+      "Special: You must have the <strong>Spell Casting</strong> feature and be trained in the skill you use to create the ward.",
+      "Roll an <strong>Arcana(Trained)</strong> or <b>Channeling(Trained)</b> check.",
+      "The DC equals 10 + (2 per lvl of the target spell), results vary by check",
+      "If you fail you do not effect the target spell.",
+      "If you succeed, you may spend spell slots equal to enemy spell level to either halve its damage, grant all targets advantage on saves, or reduce its duration by half.",
+      "If you succeed by 5 or more, you may spend spell slots equal to the enemy spell level to cancel it altogether. The enemy loses their spell.",
+    ],
+  },
+];
